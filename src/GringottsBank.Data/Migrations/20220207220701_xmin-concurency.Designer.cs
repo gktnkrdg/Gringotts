70d486790fc84f74d3d163cf4afe391c938f974d
gktnkrdg <gktnkrdg@gmail.com>
2022-02-08 01:28:30 +0300

    fix: deploy.yaml and migration

--- src/GringottsBank.Data/Migrations/20220207220701_xmin-concurency.Designer.cs
+++ src/GringottsBank.Data/Migrations/20220207220701_xmin-concurency.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using GringottsBank.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace GringottsBank.Data.Migrations
 {
     [DbContext(typeof(GringottsBankDbContext))]
-    partial class GringottsBankDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220207220701_xmin-concurency")]
+    partial class xminconcurency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GringottsBank.Data/Migrations/20220207220701_xmin-concurency.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GringottsBank.Data.Migrations
+{
+    public partial class xminconcurency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "AccountId",
+                table: "Transactions",
+                newName: "BankAccountId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_AccountId",
+                table: "Transactions",
+                newName: "IX_Transactions_BankAccountId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Customers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccounts_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_CustomerId",
+                table: "BankAccounts",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_BankAccounts_BankAccountId",
+                table: "Transactions",
+                column: "BankAccountId",
+                principalTable: "BankAccounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_BankAccounts_BankAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "BankAccountId",
+                table: "Transactions",
+                newName: "AccountId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_BankAccountId",
+                table: "Transactions",
+                newName: "IX_Transactions_AccountId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Transactions",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountNumber = table.Column<string>(type: "text", nullable: true),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CustomerID = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FieldName = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    NewValue = table.Column<string>(type: "text", nullable: true),
+                    OldValue = table.Column<string>(type: "text", nullable: true),
+                    SourceMethod = table.Column<string>(type: "text", nullable: true),
+                    TableName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_CustomerID",
+                table: "Accounts",
+                column: "CustomerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
